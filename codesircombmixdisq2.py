ae40283e2e9d6e6e55717e167f75bd4808c13c4d
Joseph Simpson <jsimpson@linfield.edu>
2020-06-18 14:50:31 -0700

    combo of population mixing with quarantine lifted and later returned - shortened and improved

--- codesircombmixdisq2.py
+++ codesircombmixdisq2.py
@@ -23,12 +23,13 @@ import scipy as sp
 import math
 import sys
 
-def mixed_quarantine_comparison(day_lifted, spacing, q1, r, q2, psd):
+def mixed_quarantine_comparison(day_lifted, spacing, psd, q1, r, q2):
     #mitigation factor is q
     #spacing is the spacing between trial runs in days
     #q1 and r are varying mitigation factors
     #q1 is the initial quarantine level (between 0 and 1)
     #r is the q value when quarantine is lifted (between 0 and 1)
+    #q2 is the second quarantine level (between 0 and 1)
     #psd is the ratio of people participating in quarantine (between 0 and 1)
     
     #initializing parameters
@@ -45,7 +46,7 @@ def mixed_quarantine_comparison(day_lifted, spacing, q1, r, q2, psd):
     # total population
     N = 1E6
     # total time (days)
-    tottime = 365
+    tottime = 200
     # initial percent removed (immune)
     pr = 0.0
     # initial percent infected
@@ -67,77 +68,53 @@ def mixed_quarantine_comparison(day_lifted, spacing, q1, r, q2, psd):
     S1[0]= psd*S[0]
     S2[0]= S[0]-S1[0]
     I[0] = pi*N
+    sum = 0
     
     #First_change is the day that restrictions are lifted
     first_change = day_lifted / dt
     
 
-    for n in range(5):
-        second_change = first_change + (spacing * n / dt)
-        qlist = []
-    
-        for i in range(niter-1):
-            if i <= first_change: 
-                q = q1
-            elif i <= second_change and i > first_change:
-                q = r
-            else:
-                q = q2
-            qlist.append(q)
-
-        for j in range(niter-1):
-            dS1dt=-qlist[j]*beta/N*S1[j]*I[j]
-            dS2dt=-beta/N*S2[j]*I[j]
-            dIdt=-dS1dt-dS2dt-gamma*I[j]
-            S1[j+1] = S1[j] + dt*dS1dt 
-            S2[j+1] = S2[j] + dt*dS2dt
-            I[j+1] = I[j] + dt*dIdt  
-        
-        S=S1+S2   
-        # plotting
-        plt.plot(t, I)
-    #no change    
-    S[0] = ps*N
-    I[0] = pi*N
-    sum = 0
-
-    for k in range(niter-1):
-       dSdt=-(beta/N)*S[k]*I[k]
-       dIdt=(beta/N)*S[k]*I[k]-gamma*I[k]
-       sum = sum + I[k]*dt
-       S[k+1] = S[k] + dt*dSdt 
-       I[k+1] = I[k] + dt*dIdt
-    plt.plot(t,I)
-    
-    #permanent q        
-    S[0] = ps*N
-    S1[0]= psd*S[0]
-    S2[0]= S[0]-S1[0]
-    I[0] = pi*N
-
-    for l in range(niter-1):
-       dS1dt=-q1*beta/N*S1[l]*I[l]
-       dS2dt=-beta/N*S2[l]*I[l]
-       dIdt=-dS1dt-dS2dt-gamma*I[l]
-       S1[l+1] = S1[l] + dt*dS1dt 
-       S2[l+1] = S2[l] + dt*dS2dt
-       I[l+1] = I[l] + dt*dIdt
-    plt.plot(t,I)
-    
-    plt.legend(['no quarantine lift', 
+    for n in range(6):
+        if n <= 4:
+            second_change = first_change + (spacing * n / dt)
+            for j in range(niter-1):
+                if j <= first_change:
+                    q = q1
+                elif j <= second_change and j > first_change:
+                    q = r
+                else:
+                    q = q2
+                dS1dt=-q*beta/N*S1[j]*I[j]
+                dS2dt=-beta/N*S2[j]*I[j]
+                dIdt=-dS1dt-dS2dt-gamma*I[j]
+                S1[j+1] = S1[j] + dt*dS1dt 
+                S2[j+1] = S2[j] + dt*dS2dt
+                I[j+1] = I[j] + dt*dIdt  
+            S=S1+S2   
+            plt.plot(t, I)
+        else:
+            for k in range(niter-1):
+                dSdt=-(beta/N)*S[k]*I[k]
+                dIdt=(beta/N)*S[k]*I[k]-gamma*I[k]
+                sum = sum + I[k]*dt
+                S[k+1] = S[k] + dt*dSdt 
+                I[k+1] = I[k] + dt*dIdt
+            plt.plot(t,I)
+    plt.legend(['no quarantine lift',
                 str(int(spacing)) + ' days relaxed', 
                 str(int(2 * spacing)) + ' days relaxed', 
                 str(int(3 * spacing)) + ' days relaxed', 
                 str(int(4 * spacing)) + ' days relaxed',
-                'no quarantine'])
+                'no quarantine'], loc='upper right')
     plt.title('Mixed SIR model lifting restrictions after ' + str(int(day_lifted))
               + ' days of quarantine \n and replacing restrictions on ' + str(int(spacing))
               + ' day intervals (I only) \n q1 = ' + str(float(q1)) + ', r = ' + str(float(r))
-              + ', q2 = ' + str(float(q2)) + ', \u03B2 = ' + str(beta) + ', \u03B3 = ' + str(gamma))
+              + ', q2 = ' + str(float(q2)) + ', \u03B2 = ' + str(beta) + ', \u03B3 = ' +
+              str(gamma) + ', psd = ' + str(float(psd)*100) + '%')
     plt.xlabel('days elapsed since 1 percent of the population became infected')
     plt.ylabel('population')
    
     return plt.show()
 
 #for m in range(10):
-mixed_quarantine_comparison(70, 14, 0.2, 1, 0.5, 0.8)
+mixed_quarantine_comparison(21, 5, 0.9, 0.3, 1, 0.3)
