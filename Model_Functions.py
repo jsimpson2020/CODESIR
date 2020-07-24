556fa164840638f9aa69bc7fa64a64cb776b41fe
Joseph Simpson <jsimpson@linfield.edu>
2020-07-24 13:46:48 -0700

    plots for presentation on endemics'

--- Model_Functions.py
+++ Model_Functions.py
@@ -17,10 +17,9 @@ import pandas as pd
 def BrauerSIR(initial_population=100000, length_of_infection=20,
               avg_contacts_per_day=0.25, total_time=365,
               initial_percent_recovered=0.0, initial_percent_infected=1.0,
-              plot=False, parameters=False):
+                 dt=0.01, plot=False, parameters=False):
     ''' Uses Euler's method to solve a simple SIR model returns plot '''
 
-    dt = 0.01
     
     lmbda = avg_contacts_per_day
     
@@ -32,6 +31,7 @@ def BrauerSIR(initial_population=100000, length_of_infection=20,
     S = np.zeros(niter)
     I = np.zeros(niter)
     R = np.zeros(niter)
+    N = np.zeros(niter)
     
     pi = initial_percent_infected/100
     pr = initial_percent_recovered/100
@@ -41,6 +41,7 @@ def BrauerSIR(initial_population=100000, length_of_infection=20,
     S[0] = ps*initial_population
     I[0] = pi*initial_population
     R[0] = pr*initial_population
+    N[0] = initial_population
     
     beta = lmbda/initial_population
     
@@ -50,14 +51,13 @@ def BrauerSIR(initial_population=100000, length_of_infection=20,
         dSdt = -beta*S[j]*I[j]
         dIdt = beta*S[j]*I[j] - alpha*I[j]
         dRdt = alpha*I[j]
+        dNdt = 0
         S[j+1] = S[j] + dt*dSdt
         I[j+1] = I[j] + dt*dIdt
         R[j+1] = R[j] + dt*dRdt
-        if S[j] < 0.01:
-            S[j+1]=0
-        if I[j] < 0.01:
+        N[j+1] = N[j] + dt*dNdt
+        if I[j] < 1:
             I[j+1]=0
-    N = S + I + R
     
     if plot == True:
         plt.plot(t, S, 'k', label = 'susceptible')
@@ -79,6 +79,7 @@ def BrauerSIR(initial_population=100000, length_of_infection=20,
               'Initial population = ' + str(initial_population) + ' people',
               'Initial percent recovered = ' +str(initial_percent_recovered) + '%',
               'Initial percent infected = ' + str(initial_percent_infected) + '%',
+              'R0 = ' + str(R0),
               sep = "\n")
     
     df = pd.DataFrame({'days':t, 'susceptible':S, 'infected':I, 'recovered':R, 'total':N})
@@ -89,11 +90,10 @@ def BrauerSIR(initial_population=100000, length_of_infection=20,
 def BrauerSEIR(initial_population=100000, length_of_infection=20,
                length_of_exposure=5, avg_contacts_per_day=0.25, total_time=365,
               initial_percent_recovered=0.0, initial_percent_infected=1.0,
-              initial_percent_exposed=0.0, plot=False, parameters=False):
+              initial_percent_exposed=0.0, dt = 0.01, plot=False, parameters=False):
     ''' Uses Euler's method to solve a simple SEIR model '''
 
-    dt = 0.01
-    
+
     lmbda = avg_contacts_per_day
     
     alpha = 1/length_of_infection
@@ -106,6 +106,7 @@ def BrauerSEIR(initial_population=100000, length_of_infection=20,
     E = np.zeros(niter)
     I = np.zeros(niter)
     R = np.zeros(niter)
+    N = np.zeros(niter)
     
     pi = initial_percent_infected/100
     pe = initial_percent_exposed/100
@@ -117,6 +118,7 @@ def BrauerSEIR(initial_population=100000, length_of_infection=20,
     E[0] = pe*initial_population
     I[0] = pi*initial_population
     R[0] = pr*initial_population
+    N[0] = initial_population
     
     beta = lmbda/initial_population
     
@@ -127,15 +129,16 @@ def BrauerSEIR(initial_population=100000, length_of_infection=20,
         dEdt=beta*S[j]*I[j]-kappa*E[j]
         dIdt=kappa*E[j]-alpha*I[j]
         dRdt=alpha*I[j]
+        dNdt=0
         S[j+1] = S[j] + dt*dSdt
         E[j+1] = E[j] + dt*dEdt
         I[j+1] = I[j] + dt*dIdt
         R[j+1] = R[j] + dt*dRdt
-        if S[j] < 0.01:
-            S[j+1]=0
-        if I[j] < 0.01:
+        N[j+1] = N[j] + dt*dNdt
+        if I[j] < 1:
             I[j+1]=0
-    N=S+E+I+R
+        if E[j] < 1 and j > 100:
+            E[j+1]=0
     
     if plot == True:
         plt.plot(t, S, 'k', label = 'susceptible')
@@ -160,6 +163,7 @@ def BrauerSEIR(initial_population=100000, length_of_infection=20,
               'Initial percent recovered = ' +str(initial_percent_recovered) + '%',
               'Initial percent infected = ' + str(initial_percent_infected) + '%',
               'Initial percent exposed = ' + str(initial_percent_exposed) + '%',
+              'R0 = ' + str(R0),
               sep = "\n")
         
     df = pd.DataFrame({'days':t, 'susceptible':S, 'exposed':E, 'infected':I,
@@ -171,10 +175,8 @@ def BrauerSEIR(initial_population=100000, length_of_infection=20,
 def BrauerSIS(initial_population=100000, length_of_infection=20,
               avg_contacts_per_day=0.25, total_time=365,
               initial_percent_recovered=0.0, initial_percent_infected=1.0,
-              fraction_recovered=1, plot=False, parameters=False):
+              fraction_recovered=1, dt = 0.01, plot=False, parameters=False):
     ''' Uses Euler's method to solve a simple SIS model returns plot '''
-
-    dt = 0.01
     
     lmbda = avg_contacts_per_day
     
@@ -187,6 +189,7 @@ def BrauerSIS(initial_population=100000, length_of_infection=20,
     S = np.zeros(niter)
     I = np.zeros(niter)
     R = np.zeros(niter)
+    N = np.zeros(niter)
     
     pi = initial_percent_infected/100
     pr = initial_percent_recovered/100
@@ -196,6 +199,7 @@ def BrauerSIS(initial_population=100000, length_of_infection=20,
     S[0] = ps*initial_population
     I[0] = pi*initial_population
     R[0] = pr*initial_population
+    N[0] = initial_population
     
     beta = lmbda/initial_population
     
@@ -205,14 +209,13 @@ def BrauerSIS(initial_population=100000, length_of_infection=20,
         dSdt = -beta*S[j]*I[j] + f*alpha*I[j]
         dIdt = beta*S[j]*I[j] - alpha*I[j]
         dRdt = (1-f)*alpha*I[j]
+        dNdt = 0
         S[j+1] = S[j] + dt*dSdt
         I[j+1] = I[j] + dt*dIdt
         R[j+1] = R[j] + dt*dRdt
-        if S[j] < 0.01:
-            S[j+1]=0
-        if I[j] < 0.01:
+        N[j+1] = N[j] + dt*dNdt
+        if I[j] < 1:
             I[j+1]=0
-    N = S + I + R
     
     if plot == True:
         plt.plot(t, S, 'k', label = 'susceptible')
@@ -235,6 +238,7 @@ def BrauerSIS(initial_population=100000, length_of_infection=20,
               'Initial population = ' + str(initial_population) + ' people',
               'Initial percent recovered = ' +str(initial_percent_recovered) + '%',
               'Initial percent infected = ' + str(initial_percent_infected) + '%',
+              'R0 = ' + str(R0),
               sep = "\n")
     
     df = pd.DataFrame({'days':t, 'susceptible':S, 'infected':I, 'recovered':R, 'total':N})
@@ -246,10 +250,8 @@ def BrauerSIRbirthdeath(initial_population=100000, length_of_infection=20,
                         avg_contacts_per_day=0.25, total_time=365,
                         initial_percent_recovered=0.0, initial_percent_infected=1.0,
                         fraction_recovered=1.0, deaths_per_100000_per_day=2.4, 
-                        births_per_100000_per_day=3.3, xmin=0, xmax=365, ymin=0,
+                        births_per_100000_per_day=3.3, dt=0.01, xmin=0, xmax=365, ymin=0,
                         ymax=105000, plot=False, parameters=False):
-        
-    dt = 0.01
     
     lmbda = avg_contacts_per_day
     
@@ -280,15 +282,18 @@ def BrauerSIRbirthdeath(initial_population=100000, length_of_infection=20,
     for j in range(niter-1):
         dSdt=nu*N[j]-lmbda/N[j]*S[j]*I[j]-mu*S[j]
         dIdt=lmbda/N[j]*S[j]*I[j]-mu*I[j]-alpha*I[j]
+        dRdt=f*alpha*I[j]-mu*R[j]
         dNdt=nu*N[j]-(1-f)*alpha*I[j]-mu*N[j]
         S[j+1] = S[j] + dt*dSdt 
         I[j+1] = I[j] + dt*dIdt
+        R[j+1] = R[j] + dt*dRdt
         N[j+1] = N[j] + dt*dNdt
-        if S[j] < 0.01:
-            S[j+1]=0
-        if I[j] < 0.01:
+        if I[j] < 1:
             I[j+1]=0
-    R = N-S-I
+        if N[j] < 1:
+            S[j+1]=0
+            I[j+1]=0
+            R[j+1]=0
     
     R0 = lmbda/(mu + alpha)
     
@@ -319,7 +324,7 @@ def BrauerSIRbirthdeath(initial_population=100000, length_of_infection=20,
               'Initial population = ' + str(initial_population) + ' people',
               'Initial percent recovered = ' +str(initial_percent_recovered) + '%',
               'Initial percent infected = ' + str(initial_percent_infected) + '%',
-              'Total population at the end of 1st year = ' + str(N[36600].round(0)),
+              'R0 = ' + str(R0approx),
               sep = "\n")
         
     df = pd.DataFrame({'days':t, 'susceptible':S, 'infected':I, 'recovered':R, 'total':N})
@@ -332,10 +337,8 @@ def BrauerSIRSbirthdeath(initial_population=100000, length_of_infection=20,
                         initial_percent_recovered=0.0, initial_percent_infected=1.0,
                         fraction_recovered=1.0, deaths_per_100000_per_day=2.4, 
                         births_per_100000_per_day=3.3, length_of_immunity=90,
-                        xmin=0, xmax=365, ymin=0, ymax=105000, plot=False,
+                        dt = 0.01, xmin=0, xmax=365, ymin=0, ymax=105000, plot=False,
                         parameters=False):
-    
-    dt = 0.01
     
     lmbda = avg_contacts_per_day
     
@@ -374,10 +377,12 @@ def BrauerSIRSbirthdeath(initial_population=100000, length_of_infection=20,
         I[j+1] = I[j] + dt*dIdt
         R[j+1] = R[j] + dt*dRdt
         N[j+1] = N[j] + dt*dNdt
-        if S[j] < 0.01:
-            S[j+1]=0
-        if I[j] < 0.01:
+        if I[j] < 1:
             I[j+1]=0
+        if N[j] < 1:
+            S[j+1]=0
+            I[j+1]=0
+            R[j+1]=0
     
     R0 = lmbda/(mu + alpha)
     
@@ -410,6 +415,7 @@ def BrauerSIRSbirthdeath(initial_population=100000, length_of_infection=20,
               'Initial population = ' + str(initial_population) + ' people',
               'Initial percent recovered = ' +str(initial_percent_recovered) + '%',
               'Initial percent infected = ' + str(initial_percent_infected) + '%',
+              'R0 = ' + str(R0approx),
               sep = "\n")
     
     df = pd.DataFrame({'days':t, 'susceptible':S, 'infected':I, 'recovered':R, 'total':N})
@@ -417,7 +423,8 @@ def BrauerSIRSbirthdeath(initial_population=100000, length_of_infection=20,
     return df
 
 def PlotSIR(t=0, S=0, I=0, R=0, N=0, legend=('susceptible', 'infected', 'recovered', 'total'),
-            legend_location= 'right', title=' ', xlabel=' ', ylabel=' '):
+            legend_location= 'right', title=' ', xlabel=' ', ylabel=' ', plotname=' ',
+            plotsave=False):
     plt.plot(t, S)
     plt.plot(t, I)
     plt.plot(t, R)
@@ -426,6 +433,8 @@ def PlotSIR(t=0, S=0, I=0, R=0, N=0, legend=('susceptible', 'infected', 'recover
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
+    if plotsave == True:
+        plt.savefig(plotname)
     plt.show()
     
     return plt.show()
